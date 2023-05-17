f1a3e7ef0ca5c4daf986a0b2a139dda7f401ec8e
titus.dishon <titus.dishon@postcodelottery.co.uk>
2023-05-17 20:03:15 +0300

    add model initialization

--- backend/src/models/product.ts
+++ backend/src/models/product.ts
@@ -1,24 +1,60 @@
-import { Column, DataType, Model, Table } from "sequelize-typescript";
+import { DataTypes, Model, Sequelize } from "sequelize";
+import sequelize from "../database-connection";
 
-@Table({
-  timestamps: false,
-  tableName: "products",
-})
-export class Products extends Model {
-  @Column({ type: DataType.STRING, allowNull: false })
-  productName!: string;
-
-  @Column({ type: DataType.INTEGER, allowNull: false })
-  quantity!: number;
-
-  @Column({ type: DataType.STRING })
-  imageUrl!: string;
-
-  @Column({ type: DataType.STRING })
-  batchNumber!: string;
-  @Column({ type: DataType.DATE })
-  createdAt!: Date;
-
-  @Column({ type: DataType.DATE })
-  updatedAt!: Date;
+interface ProductAttributes {
+  id: number;
+  productName: string;
+  quantity: number;
+  imageUrl: string;
+  batchNumber: string;
+  createdAt?: Date;
+  updatedAt?: Date;
 }
+
+class Product extends Model<ProductAttributes> implements ProductAttributes {
+  public id!: number;
+  public productName!: string;
+  public quantity!: number;
+  public imageUrl!: string;
+  public batchNumber!: string;
+  public createdAt?: Date;
+  public updatedAt?: Date;
+}
+
+Product.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    productName: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    quantity: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+    imageUrl: {
+      type: DataTypes.STRING,
+    },
+    batchNumber: {
+      type: DataTypes.STRING,
+    },
+    createdAt: {
+      type: DataTypes.DATE,
+    },
+    updatedAt: {
+      type: DataTypes.DATE,
+    },
+  },
+  {
+    sequelize,
+    tableName: "products",
+    modelName: "Product",
+    timestamps: true,
+  }
+);
+
+export default Product;
