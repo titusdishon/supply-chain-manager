2f746ebb92401e1685584112f37b03ef03743828
titus.dishon <titus.dishon@postcodelottery.co.uk>
2023-05-17 20:02:54 +0300

    change exported ame to sequelize

--- backend/src/database-connection.ts
+++ backend/src/database-connection.ts
@@ -1,15 +1,18 @@
-import { Sequelize } from "sequelize-typescript";
+import { Sequelize } from "sequelize";
 import dotenv from "dotenv";
 dotenv.config();
 
-const sequelize = new Sequelize({
-  database: `${process.env.DB_NAME}`,
-  dialect: "postgres",
-  username: `${process.env.DB_USER}`,
-  password: `${process.env.DB_PASSWORD}`,
-  host: `${process.env.DB_HOST}`,
-  port: parseInt(`${process.env.DB_PORT}`),
-  // models: [__dirname + "/models"],
-});
+const sequelize = new Sequelize(
+  `${process.env.DB_NAME}`,
+  `${process.env.DB_USER}`,
+  `${process.env.DB_PASSWORD}`,
+
+  {
+    dialect: "postgres",
+    host: `${process.env.DB_HOST}`,
+    port: parseInt(`${process.env.DB_PORT}`),
+    // models: [__dirname + "/models"],
+  }
+);
 
 export default sequelize;
